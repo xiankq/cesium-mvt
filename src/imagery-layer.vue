0eeda7f1bc290a31c1995cd6c3f0a9ccc002f773
xiankq <xiankq@qq.com>
2026-04-09 15:59:55 +0800

    1

--- src/imagery-layer.vue
+++ src/imagery-layer.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import type { Viewer } from 'cesium';
 import { watchEffect } from 'vue';
-import { MvtImageryProvider } from './mvt';
 
 const props = defineProps<{
   viewer: Viewer;
@@ -12,7 +11,7 @@ watchEffect((onCleanup) => {
     new MvtImageryProvider({
       scene: props.viewer.scene,
       source: 'openmaptiles',
-      styleUrl: 'https://tiles.openfreemap.org/styles/bright',
+      style: 'https://tiles.openfreemap.org/styles/bright',
     }),
   );
   onCleanup(() => layer && props.viewer.imageryLayers.remove(layer));
