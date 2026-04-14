d7fd6fe7372199c2b537c3051876f382226ec32f
xiankq <xiankq@qq.com>
2026-04-14 11:47:50 +0800

    1

--- src/imagery-layer.vue
+++ src/imagery-layer.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { Viewer } from 'cesium';
+import { Color } from 'cesium';
 import { watchEffect } from 'vue';
 import { CesiumVectorTile } from './mvt/cesium-vector-tile';
 
@@ -10,6 +11,8 @@ const props = defineProps<{
 const STYLE_URL = 'https://tiles.openfreemap.org/styles/liberty';
 
 watchEffect(async (onCleanup) => {
+  // eslint-disable-next-line vue/no-mutating-props
+  props.viewer.scene.globe.baseColor = Color.fromCssColorString('#f8f4f0');
   const vectorTile = await CesiumVectorTile.fromUrl(STYLE_URL);
   props.viewer.scene.primitives.add(vectorTile);
   onCleanup(() => {
